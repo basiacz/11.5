7411e82e0fb81102e1a991febfb96f0fd5fafa0c
basiacz <basiaczerw@gmail.com>
2018-12-12 22:20:48 +0100

    11.5

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ function Button(text) {
 }
 
 Button.prototype = {
-  create: function () {
+  create: function() {
     var self = this;
     this.element = document.createElement('button');
     this.element.innerText = this.text;
-    this.element.addEventListener('click', function () {
-      alert(self.text)
+    this.element.addEventListener('click', function() {
+      alert(self.text);
     });
     document.body.appendChild(this.element);
   }
-}
+};
 
 var btn1 = new Button('Hello');
 btn1.create();
